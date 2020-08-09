acd95156beff2d5610063b914045800e71fe0304
Mateo Orobio <mateo.orobio@idfive.com>
2020-08-09 03:17:27 -0500

    feat: likes update functionality

--- src/components/VoteComponent/VoteComponent.vue
+++ src/components/VoteComponent/VoteComponent.vue
@@ -2,7 +2,8 @@
   <div class="vote-module" :style="{ backgroundImage: 'url(' + imgSrc + ')' }">
     <div class="vote-module__content">
       <div class="vote-popularity__wrapper">
-        <span class="popularity-indicator" :class="{'disliked': likes < dislikes}">
+        <span class="popularity-indicator"
+        :class="{'disliked': popularityComputed.likes < popularityComputed.dislikes}">
           <img src="../../assets/img/like.svg" alt="">
         </span>
         <h2 class="vote-module__name">{{name}}</h2>
@@ -11,23 +12,30 @@
         {{description}}
       </p>
       <div class="vote-module__content__footer">
-        <form action="submit">
+        <form v-if="!voted">
           <fieldset>
             <label class="container container--like">
-              <input type="radio" name="radio">
+              <input type="radio" name="radio" value="liked" v-model="voteLiked">
               <img src="../../assets/img/like.svg" alt="">
               <span class="sr-only">Like</span>
               <span class="checkmark"></span>
             </label>
             <label class="container container--dislike">
-              <input type="radio" name="radio">
+              <input type="radio" name="radio" value="disliked" v-model="voteLiked">
               <img src="../../assets/img/like.svg" alt="">
               <span class="sr-only">Dislike</span>
               <span class="checkmark"></span>
             </label>
           </fieldset>
-          <button class="button" type="submit">Vote Now</button>
+          <button
+          class="button"
+          @click.prevent="voteLiked !== '' ? updatePopularity() : ''">
+            Vote Now
+          </button>
         </form>
+        <button v-if="voted" class="button" @click="voteAgain">
+          Vote Again
+        </button>
       </div>
     </div>
     <div class="vote-module__measurement">
@@ -35,10 +43,10 @@
       <span class="disliked" :style="{width: popularityComputed.dislikes + '%'}"></span>
       <div class="vote-module__measurement-like">
         <img src="../../assets/img/like.svg" alt="Like">
-        <p>{{popularityComputed.likes+'%'}}</p>
+        <p>{{Math.floor(popularityComputed.likes)+'%'}}</p>
       </div>
       <div class="vote-module__measurement-dislike">
-        <p>{{popularityComputed.dislikes+'%'}}</p>
+        <p>{{Math.floor(popularityComputed.dislikes)+'%'}}</p>
         <img src="../../assets/img/like.svg" alt="Like">
       </div>
     </div>
@@ -48,6 +56,14 @@
 <script>
 export default {
   name: 'Vote',
+  data() {
+    return {
+      voted: false,
+      localLikes: this.$props.likes,
+      localDislikes: this.$props.dislikes,
+      voteLiked: String,
+    };
+  },
   props: {
     id: Number,
     name: String,
@@ -59,13 +75,26 @@ export default {
   mounted() {
   },
   methods: {
+    updatePopularity() {
+      const voteStatus = this.voteLiked === 'liked';
+      this.voted = true;
+      if (voteStatus) {
+        this.localLikes += 1;
+      } else {
+        this.localDislikes += 1;
+      }
+    },
+    voteAgain() {
+      this.voted = false;
+      this.voteLiked = '';
+    },
   },
   computed: {
     popularityComputed() {
-      const rate = this.$props.likes + this.$props.dislikes;
+      const rate = this.localLikes + this.localDislikes;
       const popularityObj = {
-        likes: (this.$props.likes * 100) / rate,
-        dislikes: (this.$props.dislikes * 100) / rate,
+        likes: (this.localLikes * 100) / rate,
+        dislikes: (this.localDislikes * 100) / rate,
       };
       return popularityObj;
     },
