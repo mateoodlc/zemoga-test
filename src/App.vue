c19b4418ffc075c5686a542a8dde7070d8dbdb4a
Mateo Orobio <mateo.orobio@idfive.com>
2020-08-09 02:03:48 -0500

    feat: data json created

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,55 @@
 <template>
   <div id="app">
-    <h2>Previous Rulings</h2>
+    <div class="max-bound outer-pad">
+      <section class="vote-section">
+        <h2 class="heading--dark">Previous Rulings</h2>
+        <div class="vote-section__container">
+          <div class="vote-module__wrapper"
+            v-for="(vote, index) of votesData"
+            :index="index" :key="vote.id">
+          <vote-component
+            :id="vote.id"
+            :name="vote.name"
+            :description="vote.description"
+            :likes="vote.likes"
+            :dislikes="vote.dislikes"
+            :imgSrc="vote.imgSrc"
+          ></vote-component>
+        </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import VoteComponent from './components/VoteComponent/VoteComponent.vue';
 
 export default {
   name: 'App',
-  components: {},
+  data() {
+    return {
+      votesData: [],
+    };
+  },
+  components: {
+    VoteComponent,
+  },
+  beforeMount() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await fetch('./assets/data.json');
+        const data = await response.json();
+        console.log(data);
+        this.votesData = await data.votes;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
 
@@ -16,8 +57,5 @@ export default {
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
